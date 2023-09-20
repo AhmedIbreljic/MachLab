1d50bbc9424f1e2c944e17b86442c8abab1209b4
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-09-19 23:03:35 -0400

    Update AppImage.tsx

--- apps/landing/src/components/AppImage.tsx
+++ apps/landing/src/components/AppImage.tsx
@@ -8,9 +8,6 @@ const AppImage = () => {
 	return (
 		<div className="w-screen">
 			<div className="relative mx-auto max-w-full sm:w-full sm:max-w-[1400px]">
-				<div className="bloom burst bloom-one" />
-				<div className="bloom burst bloom-three" />
-				<div className="bloom burst bloom-two" />
 			</div>
 			<div className="fade-in-app-embed relative z-30 mt-8 h-[255px] w-full px-1 text-center sm:mt-16 sm:h-[428px] md:h-[428px] lg:h-[628px]">
 				<AppFrameOuter>
