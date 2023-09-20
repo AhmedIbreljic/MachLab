c4180ceb9f74c2c4ba21edcc0bfa733fd9c24eb1
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-09-20 15:17:49 -0400

    Update team.tsx

--- apps/landing/src/pages/team.tsx
+++ apps/landing/src/pages/team.tsx
@@ -46,113 +46,11 @@ export const teamMembers: Array<TeamMemberProps> = [
 
 const investors: Array<TeamMemberProps> = [
 	{
-		name: 'Joseph Jacks',
-		role: 'Founder, OSSC',
-		investmentRound: 'Lead Seed',
-		imageUrl: '/images/investors/josephjacks.jpg'
+		name: 'Coming soon',
+		role: 'XXX',
+		investmentRound: 'Seed Round',
+		imageUrl: '/images/investors/X.jpg'
 	},
-	{
-		name: 'Guillermo Rauch',
-		role: 'CEO, Vercel',
-		investmentRound: 'Co-Lead Seed',
-		imageUrl: '/images/investors/guillermo.jpg'
-	},
-	{
-		name: 'Naval Ravikant',
-		role: 'Founder, AngelList',
-		investmentRound: 'Co-Lead Seed',
-		imageUrl: '/images/investors/naval.jpg'
-	},
-	{
-		name: 'Neha Narkhede',
-		role: 'Confluent, Apache Kafka',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/neha.jpg'
-	},
-	{
-		name: 'Austen Allred',
-		role: 'CEO, Bloom Institute of Technology',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/austen.jpg'
-	},
-	{
-		name: 'Tom Preston-Werner',
-		role: 'Founder, GitHub',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/TOM.jpg'
-	},
-	{
-		name: 'Tobias Lütke',
-		role: 'CEO, Shopify',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/tobiaslutke.jpg'
-	},
-	{
-		name: 'Justin Hoffman',
-		role: 'Former VP Sales, Elasticsearch',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/justinhoffman.jpg'
-	},
-	{
-		name: 'Ry Walker',
-		role: 'Founder, Astronomer',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/rywalker.jpg'
-	},
-	{
-		name: 'Zachary Smith',
-		role: 'Head of Edge Infrastructure, Equinix',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/zacharysmith.jpg'
-	},
-	{
-		name: 'Sanjay Poonen',
-		role: 'Former COO, VMware',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/sanjay.jpg'
-	},
-	{
-		name: 'David Mytton',
-		role: 'CEO, console.dev',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/davidmytton.jpg'
-	},
-	{
-		name: 'Peer Richelsen',
-		role: 'CEO, Cal.com',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/peer.jpg'
-	},
-	{
-		name: 'Lester Lee',
-		role: 'Founder, Slapdash',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/lesterlee.jpg'
-	},
-	{
-		name: 'Haoyuan Li',
-		role: 'Founder, Alluxio',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/haoyuan.jpg'
-	},
-	{
-		name: 'Augusto Marietti',
-		role: 'CEO, Kong',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/augusto.jpg'
-	},
-	{
-		name: 'Vijay Sharma',
-		role: 'CEO, Belong',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/vijay.jpg'
-	},
-	{
-		name: 'Naveen R',
-		role: 'Founder, NocoDB',
-		investmentRound: 'Seed',
-		imageUrl: '/images/investors/naveen.jpg'
-	}
 ];
 
 export default function TeamPage() {
